0da86c244e5181a57e2e86db2e23828fc9f12b24
Eamonn de Leastar <edeleastar@wit.ie>
2017-12-19 16:49:06 +0000

    add actionbBar support + menu resources

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_placemark_list.*
@@ -23,10 +24,16 @@ class PlacemarkListActivity : AppCompatActivity() {
     app = application as MainApp
 
     toolbarMain.title = title
+    setSupportActionBar(toolbarMain)
 
     val layoutManager = LinearLayoutManager(this)
     recyclerView.layoutManager = layoutManager
     recyclerView.adapter = PlacemarkAdapter(app.placemarks)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return super.onCreateOptionsMenu(menu)
   }
 }
 

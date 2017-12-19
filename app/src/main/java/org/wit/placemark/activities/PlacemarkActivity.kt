75ca27c14c518997c636e3bf1fbca050f3cff514
Eamonn de Leastar <edeleastar@wit.ie>
2017-12-19 10:40:32 +0000

    introduce PlacemarkMode - use model in activity

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -7,16 +7,19 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
+
+  var placemark = PlacemarkModel()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
 
     btnAdd.setOnClickListener() {
-      val placemarkTitle = placemarkTitle.text.toString()
-      if (placemarkTitle.isNotEmpty()) {
+      placemark.title = placemarkTitle.text.toString()
+      if (placemark.title.isNotEmpty()) {
         info("add Button Pressed: $placemarkTitle")
       }
       else {

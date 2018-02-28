3fe2b04188d9b40a92f3e40547cefce4db86f071
Eamonn de Leastar <edeleastar@wit.ie>
2018-02-28 11:49:53 +0000

    place markers onto map

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -3,9 +3,13 @@ package org.wit.placemark.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.placemark.R
 import kotlinx.android.synthetic.main.activity_placemark_maps.*
 import kotlinx.android.synthetic.main.content_placemark_maps.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
 import org.wit.placemark.main.MainApp
 
 class PlacemarkMapsActivity : AppCompatActivity() {
@@ -28,6 +32,13 @@ class PlacemarkMapsActivity : AppCompatActivity() {
 
   fun configureMap() {
     map.uiSettings.setZoomControlsEnabled(true)
+    async(UI) {
+      app.placemarks.findAll().forEach {
+        val loc = LatLng(it.lat, it.lng)
+        val options = MarkerOptions().title(it.title).position(loc)
+        map.addMarker(options).tag = it.id
+      }
+    }
   }
 
   override fun onDestroy() {

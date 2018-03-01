e3bcce5aaed0308a746b68633909e4b4c9ab4613
Eamonn de Leastar <edeleastar@wit.ie>
2018-03-01 11:12:15 +0000

    initialize map control in PlacemarkActivity lifecycle

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -27,6 +27,7 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
+    mapView.onCreate(savedInstanceState);
     app = application as MainApp
 
     toolbarAdd.title = title
@@ -114,5 +115,30 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
         }
       }
     }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mapView.onDestroy()
+  }
+
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView.onLowMemory()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    mapView.onPause()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mapView.onResume()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle?) {
+    super.onSaveInstanceState(outState)
+    mapView.onSaveInstanceState(outState)
   }
 }
